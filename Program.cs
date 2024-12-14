b6703f7b722d87b9e37064d41a0d7438c07c7779
MarlonnLima <goncalvesmarlon072@gmail.com>
2024-12-14 02:21:59 -0300

    Learning about Guids

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello World");
+﻿var id = Guid.NewGuid(); // Gera um Guid Aleatório
+id = new Guid("4f269ad7-7e3d-4e3c-8cc1-0b49443af319");
+
+var id2 = new Guid(); // Gera um guid composto de zeros porque apenas inicializou a classe;
+Console.WriteLine(id);
+Console.WriteLine(id2);
