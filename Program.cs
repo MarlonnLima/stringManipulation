35d28c8aa0cd78d922d7a5ee4983384e9b391c9e
MarlonnLima <goncalvesmarlon072@gmail.com>
2024-12-14 02:37:09 -0300

    other string methods

--- Program.cs
+++ Program.cs
@@ -23,3 +23,10 @@ Console.WriteLine(texto.Equals("testando"));
 
 Console.WriteLine(texto.IndexOf("t"));
 Console.WriteLine(texto.LastIndexOf("t"));
+
+// Case Methods
+Console.WriteLine(texto.ToUpper());
+Console.WriteLine(texto.ToLower());
+Console.WriteLine(texto.Insert(5, " AQUI "));
+Console.WriteLine(texto.Remove(5, 3));
+Console.WriteLine(texto.Length);
