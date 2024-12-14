20245fc3ac02c9434d06a0d8c59ae6c503a23d6a
MarlonnLima <goncalvesmarlon072@gmail.com>
2024-12-14 02:25:35 -0300

    Learning about string format

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
-﻿var id = Guid.NewGuid(); // Gera um Guid Aleatório
+﻿// Guid
+var id = Guid.NewGuid(); // Gera um Guid Aleatório
 id = new Guid("4f269ad7-7e3d-4e3c-8cc1-0b49443af319");
 
 var id2 = new Guid(); // Gera um guid composto de zeros porque apenas inicializou a classe;
 Console.WriteLine(id);
 Console.WriteLine(id2);
+
+// Format
+Console.WriteLine(string.Format("Guid: {0}", id));
+
